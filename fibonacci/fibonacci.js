56c77204de0cb988f127ed6b61ebef843a7694ef
jaclyn <jaclyndshahan@gmail.com>
2020-06-18 15:30:59 -0500

    completed fibonacci and palindromes

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+function fibonacci(i) {
+    if (i === 0) {
+        return i
+    } else if (i === 1) {
+        return i
+    } else {
+        return fibonacci (i - 2) + fibonacci (i - 1)
+    }
 }
 
 module.exports = fibonacci
--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function() {
-
+function palindromes(str) {
+    console.log(str)
+    let splitStr = str.split("")
+    console.log(splitStr)
+    let reverseArr = splitStr.reverse()
+    console.log(reverseArr)
+    let answer = reverseArr.join("")
+    console.log(answer)
+    if (str === answer) {
+        return true
+    } else {
+        return false
+    }
 }
 
 module.exports = palindromes
