f0ea1f64aab6cea0297ee7704feda7d776ad649a
jaclyn <jaclyndshahan@gmail.com>
2020-06-18 15:37:51 -0500

    completed helloWorld and reverseString

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+function reverseString (str) {
+  let strArr = str.split('')
+  console.log(strArr)
+  let newStr = strArr.reverse()
+  console.log(newStr)
+  let answer = newStr.join('')
+  console.log(answer)
+  return answer
 }
 
 module.exports = reverseString
